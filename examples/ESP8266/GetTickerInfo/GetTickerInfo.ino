ebd03a405c014a2c94b67fde4c679a85bbef032d
Sandro Akira <smuramoto@gmail.com>
2018-01-30 19:50:20 -0200

    Updated GetTickerInfo.ino

--- examples/ESP8266/GetTickerInfo/GetTickerInfo.ino
+++ examples/ESP8266/GetTickerInfo/GetTickerInfo.ino
@@ -1,4 +1,4 @@
-#include <CoinMarketCapApi.h>
+#include <MercadoBitcoinApi.h>
 
 /*******************************************************************
  *  An example of getting ticker info from coinmarketcap.com       *
@@ -20,7 +20,7 @@ char password[] = "PASSWORD";  // your network key
 
 
 WiFiClientSecure client;
-CoinMarketCapApi api(client);
+MercadoBitcoinApi api(client);
 
 // CoinMarketCap's limit is "no more than 10 per minute"
 // Make sure to factor in if you are requesting more than one coin.
@@ -55,6 +55,7 @@ void setup() {
 void printTickerData(String ticker) {
   Serial.println("---------------------------------");
   Serial.println("Getting ticker data for " + ticker);
+  Serial.println("---------------------------------");
 
 
   // Ticker unfortunately is not the symbol for some reason.
@@ -64,50 +65,22 @@ void printTickerData(String ticker) {
 
   // Currency is optional, so you can pass only ticker if you want.
   // Check out the currency drop down on CoinMarketCap.com to get available values
-  CMCTickerResponse response = api.GetTickerInfo(ticker, "eur");
+  MBTickerResponse response = api.GetTickerInfo(ticker);
   if (response.error == "") {
-    Serial.print("ID: ");
-    Serial.println(response.id);
-    Serial.print("Name: ");
-    Serial.println(response.name);
-    Serial.print("Symbol: ");
-    Serial.println(response.symbol);
-
-    Serial.print("Rank: ");
-    Serial.println(response.rank);
-
-    Serial.print("Price in USD: ");
-    Serial.println(response.price_usd);
-    Serial.print("Price in BTC: ");
-    Serial.println(response.price_btc);
-
-    Serial.print("24h Volume USD: ");
-    Serial.println(response.volume_usd_24h);
-    Serial.print("Market Cap USD: ");
-    Serial.println(response.market_cap_usd);
-
-    Serial.print("Available Supply: ");
-    Serial.println(response.available_supply);
-    Serial.print("Total Supply: ");
-    Serial.println(response.total_supply);
-
-    Serial.print("Percent Change 1h: ");
-    Serial.println(response.percent_change_1h);
-    Serial.print("Percent Change 24h: ");
-    Serial.println(response.percent_change_24h);
-    Serial.print("Percent Change 7d: ");
-    Serial.println(response.percent_change_7d);
-    Serial.print("Last Updated: ");
-    Serial.println(response.last_updated);
-
-    // These fields will not come back if you do not request a currency
-    Serial.print("Price in requested currecy: ");
-    Serial.println(response.price_currency);
-    Serial.print("24h Volume in requested currency: ");
-    Serial.println(response.volume_currency_24h);
-    Serial.print("Market Cap in requested currency: ");
-    Serial.println(response.market_cap_currency);
-
+    Serial.print("High: ");
+    Serial.println(response.high);
+    Serial.print("Low : ");
+    Serial.println(response.low);
+    Serial.print("Last: ");
+    Serial.println(response.last);
+    Serial.print("Vol : ");
+    Serial.println(response.vol);
+    Serial.print("Buy : ");
+    Serial.println(response.buy);
+    Serial.print("Sell: ");
+    Serial.println(response.sell);
+    Serial.print("Data: ");
+    Serial.println(response.date);
 
   } else {
     Serial.print("Error getting data: ");
@@ -119,9 +92,8 @@ void printTickerData(String ticker) {
 void loop() {
   unsigned long timeNow = millis();
   if ((timeNow > api_due_time))  {
-    printTickerData("bitcoin");
-    printTickerData("ethereum");
-    printTickerData("dogecoin");
+    printTickerData("BTC");
+    printTickerData("LTC");
     api_due_time = timeNow + api_mtbs;
   }
 }
